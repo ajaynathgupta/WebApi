51f24a6c3eb6891bd2eba3d0670d3f8fc7181d04
Ajay <ajaynathgupta@gmail.com>
2018-11-11 12:32:48 +0530

    Add project files.

---
+++ WebApi/Controllers/EmpController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    public class EmpController : ApiController
+    {
+        TESTEntitiesContext TestDB = new TESTEntitiesContext();
+        //GET api/values
+        public IEnumerable<EMP> Get()
+        {
+
+            return TestDB.EMPs.AsEnumerable().ToList();
+        }
+
+    }
+}
